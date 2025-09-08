7fc5203fe3b6efa803f3854155769a30659da8c9
Benevanio <benevaniosantos930@gmail.com>
2025-09-08 15:12:35 -0300

    feat: define IRepositoriesContracts interface with CRUD methods for repository operations

--- src/shared/domain/entities/repositories/Irepositories-contracts.ts
+++ src/shared/domain/entities/repositories/Irepositories-contracts.ts
@@ -1,1 +1,9 @@
-export interface IRepositoriesContracts {}
+import { Entity } from "../entity";
+
+export interface IRepositoriesContracts<E extends Entity> {
+  findById(id: string): Promise<E | null>;
+  findAll(): Promise<E[]>;
+  create(entity: E): Promise<E>;
+  update(id: string, entity: E): Promise<E | null>;
+  delete(id: string): Promise<boolean>;
+}
