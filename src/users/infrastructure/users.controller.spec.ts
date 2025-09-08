d7ae9a5f91c497b69300587abbd4947e1b310dd9
Benevanio <benevaniosantos930@gmail.com>
2025-09-08 15:01:26 -0300

    test: enhance UsersController tests with additional service checks and user operations

--- src/users/infrastructure/users.controller.spec.ts
+++ src/users/infrastructure/users.controller.spec.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
@@ -16,5 +17,54 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have UsersService defined', () => {
+    const service = module.get<UsersService>(UsersService);
+    expect(service).toBeDefined();
+  });
+
+  it('should call UsersService methods', async () => {
+    const service = module.get<UsersService>(UsersService);
+    const findAllSpy = jest.spyOn(service, 'findAll').mockResolvedValue([]);
+    const result = await controller.findAll();
+    expect(findAllSpy).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+  it('should create a user', async () => {
+    const service = module.get<UsersService>(UsersService);
+    const createSpy = jest.spyOn(service, 'create').mockResolvedValue({
+      id: 1,
+      name: 'John Doe',
+      email: 'bene@gmail.com',
+      password: 'securePass123',
+      createAt: new Date(),
+    });
+    const result = await controller.create({
+      name: 'John Doe',
+      email: 'bene@gmail.com',
+      password: 'securePass123',
+    });
+    expect(createSpy).toHaveBeenCalled();
+    expect(result).toEqual({
+      id: 1,
+      name: 'John Doe',
+      email: 'bene@gmail.com',
+      password: 'securePass123',
+      createAt: expect.any(Date),
+    });
+
+  });
+
+  //delete
+  it('should delete a user', async () => {
+    const result = await controller.remove('1');
+    expect(result).toBe('This action removes a #1 user');
+  });
+
+  //update
+  it('should update a user', async () => {
+    const result = await controller.update('1', { name: 'Jane Doe' });
+    expect(result).toBe('This action updates a #1 user');
   });
 });
