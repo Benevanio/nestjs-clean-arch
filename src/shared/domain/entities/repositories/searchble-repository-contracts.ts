4bfcaa9065499db45c0f2a6be56baf1ccaa45ba5
Benevanio <benevaniosantos930@gmail.com>
2025-09-09 19:46:43 -0300

    refactor: adjust formatting of ISearchableRepositoryContracts interface for improved readability

--- src/shared/domain/entities/repositories/searchble-repository-contracts.ts
+++ src/shared/domain/entities/repositories/searchble-repository-contracts.ts
@@ -1,6 +1,7 @@
 import { Entity } from "../entity";
 import { IRepositoriesContracts } from "./Irepositories-contracts";
 
-export interface ISearchableRepositoryContracts <E extends Entity, SearchInput, SearchOutput> extends IRepositoriesContracts<E> {
+export interface ISearchableRepositoryContracts <E extends Entity, SearchInput, SearchOutput>
+extends IRepositoriesContracts<E> {
   search(input: SearchInput): Promise<SearchOutput>;
 }
