7cc8507413dca4bb33b4dd2ed33eb0bf180921a9
Benevanio Santos <benevanioconceicao.sysmap@natura.net>
2025-09-30 08:20:15 -0300

    feat: implement SignupUseCase with input validation and user creation logic

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,3 +1,6 @@
+import { UserEntity } from "@/users/domain/entities/user.entity";
+import { UserRepositories } from "@/users/domain/repositories/user.repositories";
+import { BadRequestError } from "../errors/bad-request-error";
 
 export namespace SignupUseCase {
   export type Input = {
@@ -11,12 +14,32 @@ export namespace SignupUseCase {
     name: string;
     email: string;
     password: string;
-    createAt: Date;
+    createAt?: Date;
   };
 
 
   export class UseCase {
+    constructor(
+      private userReporsitory: UserRepositories.Repository
+    ) { }
     async execute(input: Input): Promise<Output> {
+
+      const { email, password, name } = input;
+
+      if (!email || !password || !name) {
+        throw new BadRequestError('Name, email and password are required');
+      }
+
+      const user = new UserEntity({ name, email, password });
+      await this.userReporsitory.insert(user);
+      const props = user.toJSON().props;
+      return {
+        id: user.id,
+        name: props.name,
+        email: props.email,
+        password: props.password,
+        createAt: props.createAt,
+      };
 
     }
   }
